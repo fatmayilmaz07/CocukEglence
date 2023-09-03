576282af926e66e3c37590d12266a5f649f62499
Fatma <fatmayilmaz7207@gmail.com>
2023-09-03 18:09:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cocukeglence/IcerikDetay.kt
@@ -0,0 +1,6 @@
+package com.example.cocukeglence
+
+import java.io.Serializable
+
+data class IcerikDetay(var icerik_detay_id:Int,var icerik_detay:String): Serializable {
+}
---
+++ app/src/main/java/com/example/cocukeglence/Icerikler.kt
@@ -0,0 +1,11 @@
+package com.example.cocukeglence
+
+import java.io.Serializable
+
+data class Icerikler(var icerik_id:Int,
+                     var icerik_ad:String,
+                     var icerik_isim:String,
+                     var icerik_resim:String,
+                     var kategoriler: Kategoriler,
+                     var icerikDetay: IcerikDetay) : Serializable {
+}
